89989019ebb53b2baf04455e2b854462306ee27c
SunWoo Kim <58077093+sue1457@users.noreply.github.com>
2021-08-05 22:53:29 +0900

    Add files via upload

---
+++ precache-manifest.2b5ef02b2d7ae4f63af51bdc85edd719.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c4a4a179b753cab84b",
+    "url": "/instagram/css/app.818182ed.css"
+  },
+  {
+    "revision": "ac3eaf471cfd8dad5bd841fd9f6346b8",
+    "url": "/instagram/img/logo2.ac3eaf47.png"
+  },
+  {
+    "revision": "4b19b7d1bdcabfbf9a8158f7a2ee2792",
+    "url": "/instagram/index.html"
+  },
+  {
+    "revision": "91c4a4a179b753cab84b",
+    "url": "/instagram/js/app.ab57818e.js"
+  },
+  {
+    "revision": "19392f49dc7729de5038",
+    "url": "/instagram/js/chunk-vendors.99207be9.js"
+  },
+  {
+    "revision": "6a1ad00dd7d473f746f3897d0ce8c52e",
+    "url": "/instagram/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/instagram/robots.txt"
+  }
+]);
